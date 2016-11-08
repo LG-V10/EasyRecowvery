c9a826026d35f95767d428cd8d98b8f5b5ebf94b
Brian Ziemek <bziemek335@gmail.com>
2016-11-08 14:02:12 -0500

    Version bump

--- EasyRecowvery.cmd
+++ EasyRecowvery.cmd
@@ -23,7 +23,7 @@ echo ==                        https://github.com/bziemek/EasyRecowvery         
 echo ==----------------------------------------------------------------------------------------==
 echo ==                      Powered by jcadduono's Dirtycow root exploit                      ==
 echo ==                 https://github.com/jcadduono/android_external_dirtycow                 ==
-echo ================================================================================ 1.3b2 =====
+echo ================================================================================ 1.3b3 =====
 echo.
 echo Pre-flight checklist:
 echo - Install ADB, perferably with the Android SDK provided by Google (https://goo.gl/7ijkjp)
